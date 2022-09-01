5ac37d64b35eac5b5d54515a2e69bac35aaebed0
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-31 23:16:18 -0400

    awnsered questions 1-10 testing conditions

---
+++ Testing sets of conditions.js
@@ -0,0 +1,53 @@
+// Type the operator that means and.
+
+a && b;
+
+// Type the operator that means or.
+
+a || b;
+
+// When both sides of an operator have to be true to pass the test, what operator is it? Type it.
+
+a && b;
+
+// When, if either or both sides of an operator is (are) true, the test passes, what operator is it? Type it.
+
+a || b;
+
+// Create the first line of an if statement that tests whether both are true: firstName is "Bugs" and lastName is "Bunny".
+
+if (firstName === "Bugs" && lastName === "Bunny") {
+    alert("Im Bugs Bunny!");
+}
+
+// Code the first line of an if statement that tests whether both are true: a has the same value as b and c has the same value as d.
+
+if (a === b && c === d) {
+    alert("Are Equal");
+}
+
+// Code the first line of an if statement that tests whether either or both are true: a has the same value as b or c doesn't have the same value as d.
+
+if (a === b || c !== d) {
+    alert("");
+}
+
+// Code the first line of an if statement that tests whether both are true: a first variable equals a second variable and also equals a third variable.
+
+if (a === b && a === c) {
+    alert("");
+}
+
+// Code the first line of an if statement that tests whether either is true: a variable is less than or equal to a particular number or whether that same variable is greater than or equal to a second number.
+
+if (a <= 1 || a >= 2){
+    alert("");
+}
+
+// Code the first line of an if statement that tests whether (1) name is either "Ace" or "Bud" and (2) age is greater than 60.
+
+if ((name === "Ace" || name === "Bud") && age > 60) {
+
+}
+
+// re code to test
