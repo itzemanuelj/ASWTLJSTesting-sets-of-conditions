5ac5c6a7f9def4b8298791822d69e6343553926a
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-31 23:56:03 -0400

    mvp commit for testing safe sets of conditions

--- Testing sets of conditions.js
+++ Testing sets of conditions.js
@@ -46,8 +46,81 @@ if (a <= 1 || a >= 2){
 
 // Code the first line of an if statement that tests whether (1) name is either "Ace" or "Bud" and (2) age is greater than 60.
 
-if ((name === "Ace" || name === "Bud") && age > 60) {
-
+if ((name === "Ace" || name === "Bud"  ) && age > 60) {
+    alert();
 }
 
-// re code to test
+// Code the first line of an if statement that tests whether (1) name is "Ace" and age is greater than 60 or (2) name is "Bud".
+
+if ((name === "Ace" && age > 60) || name === "Bud") {
+console.log();
+}
+
+// Code the first line of an if statement that tests whether (1) name is "Ace" and age is greater than 60 or (2) name is "Bud" or age is under 6.
+
+if ((name === "Ace" >= 60) || name === "Bud" || age < 6) {
+    alert();
+}
+
+// Code the first line of an if statement that tests whether both are true: a first variable equals a second variable and also equals a third variable.
+
+if (a === b && a === c) {
+//statement
+}
+
+// Code the first line of an if statement that tests whether either is true: a variable is less than or equal to a particular number or whether that same variable is greater than or equal to a second number.
+
+if (a <= 10 || a >= 12) {
+    //statement
+}
+
+// Code the first line of an if statement that tests whether (1) name is either "Ace" or "Bud" and (2) age is greater than 60.
+
+if ((name === "Ace" || name === "Bud") || age > 60) {
+    //statement
+}
+
+// Code the first line of an if statement that tests whether (1) name is "Ace" and age is greater than 60 or (2) name is "Bud".
+
+if ((name === "Ace" && age >= 60) || name === "Bud") {
+    //statement
+}
+
+// Code the first line of an if statement that tests whether (1) name is "Ace" and age is greater than 60 or (2) name is "Bud" or age is under 6.
+
+if ((name === "Ace" && age > 60) || name === "Bud"|| age < 6) {
+    //statement
+}
+
+// Code an if statement that tests whether (1) a is less than b or (2) c is greater than d or c is equal to e. If so, display an alert.
+
+if (a < b || (c > d  || c === e )) {
+    alert("");
+}
+
+// Declare two variables and assign them number values.
+// If the first variable is less than the second variable or greater than the second variable, display an alert.
+// Click the Run It button to run your code live.
+
+a = 1;
+b = 2;
+
+if (a < b || a > b){
+    alert("")
+}
+
+
+// Declare 2 variables. Assign one of them your first name and the other one your last name.
+// Code 2 prompts, asking for your first and your last name.
+// If your answers match the two variables, display an alert.
+// Click the Run It button to run your code live.
+
+name = "Justin";
+lastName = "Jones";
+
+name = prompt("Enter First Name")
+lastName = prompt("Enter Lart Name")
+
+if (name === "Justin" && lastName === "Jones") {
+    alert("");
+}
